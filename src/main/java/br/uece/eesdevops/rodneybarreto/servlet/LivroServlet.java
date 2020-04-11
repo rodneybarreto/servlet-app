6773c107fde9ff714c08e22e0aac66fc870199bb
Rodney Barreto <rwbarreto@gmail.com>
2020-04-11 17:05:06 -0300

    Implementação do CRUD

--- src/main/java/br/uece/eesdevops/rodneybarreto/servlet/LivroServlet.java
+++ src/main/java/br/uece/eesdevops/rodneybarreto/servlet/LivroServlet.java
@@ -4,6 +4,7 @@ import br.uece.eesdevops.rodneybarreto.model.Livro;
 import br.uece.eesdevops.rodneybarreto.util.JpaUtil;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -26,14 +27,102 @@ public class LivroServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        List<Livro> livros = entityManager
-                .createQuery("select l from Livro l", Livro.class)
-                .getResultList();
+        String forward = "";
+        String action = req.getParameter("action");
+        String livroId = req.getParameter("livroId");
 
-        req.setAttribute("livros", livros);
+        if ("remove".equals(action) && (livroId != null && !livroId.equals(""))) {
+            Long id = Long.parseLong(livroId);
+            this.delete(id);
 
+            req.setAttribute("livros", this.lista());
+            forward = "/livro.jsp";
+        }
+        else if ("edita".equals(action) && (livroId != null && !livroId.equals(""))) {
+            Long id = Long.parseLong(livroId);
+            Livro livro = entityManager.find(Livro.class, id);
+
+            req.setAttribute("livro", livro);
+            forward = "/form.jsp";
+        }
+        else if ("inclui".equals(action)) {
+            forward = "/form.jsp";
+        }
+        else {
+            req.setAttribute("livros", this.lista());
+            forward = "/livro.jsp";
+        }
+
+        RequestDispatcher rd = req.getRequestDispatcher(forward);
+        rd.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String livroId = req.getParameter("id");
+        Livro livro = null;
+
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try {
+            transaction.begin();
+
+            if (livroId != null && !"".equals(livroId)) {
+                Long id = Long.parseLong(livroId);
+
+                livro = entityManager.find(Livro.class, id);
+                livro.setTitulo(req.getParameter("titulo"));
+                livro.setAutor(req.getParameter("autor"));
+                livro.setResumo(req.getParameter("resumo"));
+                livro.setAnoLancamento(req.getParameter("anoLancamento"));
+            }
+            else {
+                livro = new Livro();
+                livro.setTitulo(req.getParameter("titulo"));
+                livro.setAutor(req.getParameter("autor"));
+                livro.setResumo(req.getParameter("resumo"));
+                livro.setAnoLancamento(req.getParameter("anoLancamento"));
+            }
+            entityManager.persist(livro);
+            entityManager.flush();
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Não foi possível salvar o livro: "+ e.getMessage());
+        }
+
+        req.setAttribute("livros", this.lista());
         RequestDispatcher rd = req.getRequestDispatcher("/livro.jsp");
         rd.forward(req, resp);
+    }
+
+    private List<Livro> lista() {
+        return entityManager
+                .createQuery("select l from Livro l", Livro.class)
+                .getResultList();
+    }
+
+    private void delete(Long livroId) {
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try {
+            transaction.begin();
+
+            Livro livro = entityManager.find(Livro.class, livroId);
+            entityManager.remove(livro);
+            entityManager.flush();
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Não foi possível deletar o livro: "+ e.getMessage());
+        }
     }
 
     @Override
--- src/main/webapp/livro.jsp
+++ src/main/webapp/livro.jsp
@@ -2,12 +2,13 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Livro</title>
-</head>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Livro</title>
+    </head>
 <body>
-    <h1>Livros</h1>
+    <a href="livraria?action=inclui">+ Novo</a>
+    <h4>Livros</h4>
     <table border="1">
         <thead>
             <tr>
@@ -28,10 +29,10 @@
                     <td>${livro.resumo}</td>
                     <td>${livro.anoLancamento}</td>
                     <td>
-                        <a href="LivroServlet?action=edita&livroId=${livro.id}">Editar</a>
+                        <a href="livraria?action=edita&livroId=${livro.id}">Editar</a>
                     </td>
                     <td>
-	                    <a href="LivroServlet?action=remove&livroId=${livro.id}">Remover</a>
+	                    <a href="livraria?action=remove&livroId=${livro.id}">Remover</a>
                     </td>
                 </tr>
             </c:forEach>
